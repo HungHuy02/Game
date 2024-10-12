38000d8eb7835d7c93f5ee2baa1de2b0a735cfd3
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2024-10-12 21:57:50 +0700

    implement redis realtime ranking

---
+++ server/config/redisConfig.js
@@ -0,0 +1,19 @@
+const { createClient } = require('redis');
+
+const client = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
+
+client.on('connect', function () {
+    console.log('connected redis success!!!')
+});
+  
+client.on('error', err => console.log('Redis Client Error', err)); 
+
+client.connect();
+
+module.exports = client;
---
+++ server/controllers/restful/rank.js
@@ -0,0 +1,51 @@
+const redisClient = require("../../config/redisConfig");
+
+const REDIS_KEY = 'rank';
+const REDIS_DATA_KEY = 'rank_data';
+
+const updateUserScore = async (userId, score, userName) => {
+    await redisClient.zAdd(REDIS_KEY, [{score: score, value: `${userId}`}]);
+    if(!userName) return;
+    await redisClient.hSetNX(REDIS_DATA_KEY, `${userId}`, userName);
+};
+
+const getUserRanking = async (userId) => {
+    const userRank = await redisClient.zRevRank(REDIS_KEY, `${userId}`);
+    return userRank + 1;
+};
+
+const getRank = async (limit) => {
+    try {
+        const result = [];
+        const userRankingSet = await redisClient.zRange(REDIS_KEY, 0, limit, 'WITHSCORES');
+        const topUserScore = userRankingSet.filter((value, index) => {
+            if(index % 2 === 1) return value;
+            return false;
+        });
+
+        const topUserId = userRankingSet.filter((value, index) => {
+            if(index % 2 === 0) return value;
+            return false;
+        });
+
+        const listUserName = await redisClient.hmGet(REDIS_DATA_KEY, ...topUserId);
+
+        for(let i = 0; i < topUserScore.length; i++) {
+            result.push({
+                id: topUserId[i],
+                ranking: i + 1,
+                name: listUserName[i],
+                score: topUserScore[i]
+            });
+        }
+        return result;
+    }catch (error) {
+        return [];
+    }
+}
+
+module.exports = {
+    updateUserScore,
+    getUserRanking,
+    getRank
+}
